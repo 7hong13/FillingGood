e5ef9cc7b5cb1334b2e45d83532ce4659a6847fb
wlrud753 <jimmy2829@naver.com>
2019-12-01 23:14:21 +0900

    12.1 DB 소스

--- src/FillingGood/app/src/main/java/com/example/fillinggood/Boundary/DBboundary/DBmanager.java
+++ src/FillingGood/app/src/main/java/com/example/fillinggood/Boundary/DBboundary/DBmanager.java
@@ -1,4 +1,37 @@
 package com.example.fillinggood.Boundary.DBboundary;
+import android.util.Log;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Connection;
 
 public class DBmanager {
+    private static DBmanager instance = new DBmanager();
+
+    public static DBmanager getInstance(){
+        return instance;
+    }
+    public DBmanager(){}
+
+    private static String IP_ADDRESS = "";
+    private static String phpName = "";
+
+    public String Connect(){
+        HttpURLConnection urlConnection = null;
+        String address = IP_ADDRESS + "\\" + phpName + ".php";
+
+        try {
+            URL url = new URL(address);
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.setReadTimeout(5000);
+            urlConnection.setConnectTimeout(5000);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.connect();
+        } catch (Exception e){
+            Log.d(phpName, "Connection Fail", e);
+            return "Fail";
+        }
+        return "Connect";
+    }
 }
