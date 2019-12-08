4169a1f1a23b91554b95c55206d3928565196596
7hong13 <ledi000@naver.com>
2019-12-08 19:51:40 +0900

    12.08 개인일정관리 scenario 추가

---
+++ src/FillingGood/app/src/main/java/com/example/fillinggood/Boundary/GroupScheduleInfoList.java
@@ -0,0 +1,9 @@
+package com.example.fillinggood.Boundary;
+
+import com.example.fillinggood.Entity.GroupScheduleInfo;
+
+import java.util.ArrayList;
+
+public class GroupScheduleInfoList {
+    public static ArrayList<ArrayList<GroupScheduleInfo>> gsInfoList = new ArrayList<>();
+}
